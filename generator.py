4a3b1f36046bdd318a70d1fc131ba01d14c7f712
vc64 <vchen2022@spyponders.com>
2022-06-13 16:26:00 -0400

    messing around with reaction parameters and methods, current model diverges

--- generator.py
+++ generator.py
@@ -16,7 +16,9 @@ from matplotlib.animation import FuncAnimation
 
 fig = plt.figure(figsize=(7, 7))
 
-shape = np.array([100, 100])
+shape = np.array([20, 20])
+
+np.seterr('raise')
 
 # grid = np.zeros(shape+1)
 # ones = np.ones(shape+1)
@@ -25,7 +27,8 @@ replace_rate = 0.1
 # grid = np.random.choice([0, 1], size=shape+1, p=((1 - replace_rate), replace_rate))
 
 rng = default_rng()
-grid = rng.uniform(0.0, 1.0, shape+1)
+grid_active = np.around(rng.uniform(0.0, 1.0, shape+1), decimals = 5)
+grid_inhib = np.around(rng.uniform(0.0, 1.0, shape+1), decimals = 5)
 
 def diffuse(row, col, grid):
     curr = grid[row, col]
@@ -50,7 +53,13 @@ def diffuse(row, col, grid):
 
     edges = grid[row, col-1] + grid[row, col+1] + grid[row-1, col] + grid[row+1, col]
     corners = grid[row-1, col-1] + grid[row-1, col+1] + grid[row+1, col-1] + grid[row+1, col+1]
-    total = edges * 0.1875 + corners * 0.0625 - 1 * curr
+    # total = edges * 0.1875 + corners * 0.0625 - 1 * curr
+    
+    diffuse_ratio = 0.1 * np.array([0.1875, 0.0625, 1.5])
+    
+    total = edges * diffuse_ratio[0] + corners * diffuse_ratio[1] - curr * diffuse_ratio[2]
+
+
 
     return total
 
@@ -62,28 +71,49 @@ def diffuse(row, col, grid):
 #     # not sure how to do this
 
 
-def grown():
-    grow_rate = 0.002
-    if rng.random(1) <= grow_rate:
-        return 0.4
-    return 0
+def changePeriodic(row, col, grid, rate):
+    # return 0
+    # curr = np.around(grid[row, col], decimals = 5)
+    # # print(rate, curr)
+    # try:
+    #     return np.around(rate * curr * (1 - curr), decimals = 5)
+    # except:
+    #     print(curr)
+    return rate * (0.9 - grid[row, col])
+
+    # if rng.random(1) <= rate:
+    #     return 0
+    # return 0
 
 
-def update(frame_num, grid, img):
-    new_grid = grid.copy()
+prev = 1
+def update(frame_num, gridA, gridI, img):
+    new_grid_active = np.around(gridA, decimals = 5).copy()
+    new_grid_inhib = np.around(gridI, decimals = 5).copy()
+    # grid = gridA.copy()
     for row in range(1, shape[0]):
         for col in range(1, shape[1]):
-            new_grid[row, col] += diffuse(row, col, grid) + grown()
-    
-    # print(new_grid[45, 56])
-    grid[:] = new_grid[:]
-    img.set_data(new_grid[1:-1, 1:-1])
+            new_grid_active[row, col] += diffuse(row, col, gridA) + changePeriodic(row, col, gridA, 0.02)
+            new_grid_inhib[row, col] += diffuse(row, col, gridI) - changePeriodic(row, col, gridI, 0.05)
+
+            rxn = min(gridA[row, col], gridI[row, col] / 2)
+            gridA[row, col] -= rxn
+            gridI[row, col] += rxn * 1.5
+    print(np.amax(new_grid_active))
+    if np.amax(new_grid_active) > prev:
+        np.new_grid_active.reshape(25,4).tolist()
+
+    gridA[:] = np.around(new_grid_active[:], decimals = 5)
+    gridI[:] = np.around(new_grid_inhib[:], decimals = 5)
+
+    # grid = new_grid_active * (new_grid_inhib ** 2)
+    img.set_data(new_grid_active[1:-1, 1:-1])
 
     return img,
 
 animation_rate = 50
 
-img = plt.imshow(grid[1:-1, 1:-1], cmap = "inferno", interpolation = "nearest")
-animation = FuncAnimation(fig, update, fargs = (grid, img,), interval = animation_rate, frames = 10)
+img = plt.imshow(grid_active[1:-1, 1:-1], cmap = "inferno", interpolation = "nearest")
+animation = FuncAnimation(fig, update, fargs = (grid_active, grid_inhib, img,), interval = animation_rate, frames = 10)
 plt.show()
 
